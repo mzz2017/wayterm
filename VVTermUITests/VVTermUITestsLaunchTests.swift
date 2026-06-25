fcd468bf3c30618e67fa163de32ed55bddef5897
mzz2017 <2017@duck.com>
2026-06-25 08:15:38 -0700

    test: bound UI launch smoke coverage

--- VVTermUITests/VVTermUITestsLaunchTests.swift
+++ VVTermUITests/VVTermUITestsLaunchTests.swift
@@ -10,7 +10,7 @@ import XCTest
 final class VVTermUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
+        false
     }
 
     override func setUpWithError() throws {
